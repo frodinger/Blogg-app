1a04180bf9970dd2943801d436de0ef22460afae
frodinger <frej.hedlin@gmail.com>
2024-05-16 12:26:06 +0200

    css

--- part-1/src/components/Navigation.jsx
+++ part-1/src/components/Navigation.jsx
@@ -7,21 +7,23 @@ const Navigation = () => {
 
   return (
     <nav>
-        <li className='list-none text-green-800'><Link to="/">Hemma Glädje</Link></li>
-      <ul>
-        <li><Link to="/">Hem</Link></li>
-        <li><Link to="/blogg">Blogg</Link></li>
-      </ul>
-      <div>
-        {user ? (
-          <>
+        <li className='list-none text-lime-700 text-lg font-serif font-semibold'><Link to="/">Hemma Glädje</Link></li>
+        <div className='flex flex-row'>
+          <ul className='flex flex-row'>
+            <li><Link to="/">Hem</Link></li>
+            <li><Link to="/blogg">Blogg</Link></li>
+          </ul>
+          <div>
+            {user ? (
+            <>
             <span>{user.name}</span>
             <button onClick={logout}>Logga ut</button>
-          </>
-        ) : (
-          <button onClick={login}>Logga in</button>
-        )}
-      </div>
+            </>
+            ) : (
+            <button onClick={login}>Logga in</button>
+            )}
+          </div>
+        </div>
     </nav>
   );
 };
--- part-1/src/pages/HomePage.jsx
+++ part-1/src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ const HomePage = () => {
 
   return (
     <div>
-      <h1>About</h1>
+      <h1>Välkommen till Hemma Glädje!</h1>
     </div>
   );
 };
